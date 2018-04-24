b85757df76e1c13d397f35933164987d9d17511e
Samuel Chen <chenjianming084004@163.com>
2018-04-24 10:58:42 +0800

    orderscene without start-rating

--- src/scene/Order/OrderScene.js
+++ src/scene/Order/OrderScene.js
@@ -3,6 +3,7 @@ import {
     Text,
     StyleSheet,
     View,
+    Image,
     TouchableOpacity
 } from 'react-native'
 import ItemOfOrder from './ItemOfOrder'
@@ -31,17 +32,88 @@ class OrderScene extends PureComponent<Props, State> {
     })
 
     render() {
+        let orderList = [];
+        for (i = 0; i < 4; i++) {
+            (this.orderinfo.status == 0) ? 
+                tobeScored = (<Text style={orderSceneStyle.isScored}>待评价</Text>) :
+                tobeScored = (<Text style={orderSceneStyle.isScored}>已评价</Text>);
+            orderList.push(
+                <View key={i} style={orderSceneStyle.scene}>
+                    <TouchableOpacity onPress={(info)=>{
+                        this.props.navigation.navigate('ItemOfOrder',{info:info});
+                    }}>
+                        <View style={orderSceneStyle.itemTitle}>
+                            <Text> {this.orderinfo.name} </Text>
+                            {tobeScored}
+                        </View>
+                        {this.lines}
+                        <View style={orderSceneStyle.mainView}>
+                            <Image source={require("../../img/payforbill/icon.png")} style={orderSceneStyle.iconImg}/>
+                            <View>
+                                <Text>{ this.orderinfo.cost }</Text>
+                                <Text>{ this.orderinfo.time }</Text>
+                                <Text>stars</Text>
+                            </View>
+                            <View style={orderSceneStyle.arrow}>
+                                <Text> > </Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            )
+        }
+
         return (
             <View>
-                <TouchableOpacity onPress={(info)=>{
-                    this.props.navigation.navigate('ItemOfOrder',{info:info});
-                }}>
-                    <Text>pay for bill</Text>
-                </TouchableOpacity>
+                { orderList }
             </View>
         ) // return
-
     }
+    orderinfo = {
+        name: "海底捞火锅(珠影广场)",
+        cost: "￥331.0",
+        time: "2017-01-08 17:05:24",
+        status: 0
+    }
+
+    lines = 
+        <Text style={orderSceneStyle.devideLine}></Text>;
 }
+
+var orderSceneStyle = StyleSheet.create({
+
+    scene: {
+        margin: 10,
+        padding:5
+    },
+    isScored: {
+        color: "red",
+        fontSize: 12
+    },
+    devideLine: {
+        borderWidth:1,
+        height: 1,
+        borderColor: "#cccccc",
+        marginVertical: 5
+    },
+    itemTitle: {
+        justifyContent: "space-between",
+        flexDirection: "row",
+    },
+    iconImg: {
+        width: 60,
+        height: 60
+    },
+    mainView: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    arrow: {
+        width: 60,
+        height: 60,
+        alignItems: "center",
+        justifyContent: "center",
+    }
+})
 
 export default OrderScene
